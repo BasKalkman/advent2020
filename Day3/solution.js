1e46cd5591983eff73684b8e28d58d2825cef89f
Bas K <39069678+Basje12@users.noreply.github.com>
2020-12-03 14:00:08 +0100

    Day 3

---
+++ Day3/solution.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const { posix } = require('path');
+
+const forrest = fs
+    .readFileSync('./input.txt', 'utf-8')
+    .split('\r\n')
+    .map((e) => e.split(''));
+
+function checkSlope(xIncrement, yIncrement) {
+    let pos = {
+        x: 0,
+        y: 0,
+    };
+    let treesEncountered = 0;
+    forrestWidth = forrest[0].length;
+
+    while (pos.y < forrest.length) {
+        if (forrest[pos.y][pos.x % forrestWidth] === '#') {
+            treesEncountered++;
+        }
+        pos.x += xIncrement;
+        pos.y += yIncrement;
+    }
+
+    return treesEncountered;
+}
+
+// Part 1
+console.log('Part 1: ', checkSlope(3, 1));
+
+// Part 2
+const slopesToCheck = [
+    [1, 1],
+    [3, 1],
+    [5, 1],
+    [7, 1],
+    [1, 2],
+];
+const result = slopesToCheck.reduce((a, c) => {
+    let [x, y] = c;
+    if (a === 0) {
+        return checkSlope(x, y);
+    } else {
+        return a * checkSlope(x, y);
+    }
+}, 0);
+console.log(result);
