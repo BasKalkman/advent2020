41b65055b062122271123aa72e5bf79ddf413710
Bas K <39069678+Basje12@users.noreply.github.com>
2020-12-08 10:58:43 +0100

    Day 7

--- Day7/solution.js
+++ Day7/solution.js
@@ -3,22 +3,28 @@ const data = fs.readFileSync('./input.txt', 'utf-8').split('\r\n')
 
 const bagRules = {}
 
-
-function processRuleLine(line) {
-    [bag1, ...bags] = line.split('contain')
+for (let i = 0; i < data.length; i++) {
+    let [bag1, ...bags] = data[i].split('contain');
     bag1 = bag1.replace(/[0-9.]|\sbags?.?[^a-zA-Z]/g, '')
 
-    let bagsAllowed = bags[0].split(',').map(e => {
-        return e.replace(/[0-9.]/g, '').replace(/bags?/g,'').trim()
+    let contains = bags[0].split(',').map(e => {
+        if (e.match(/no other bags/)) {
+            return null
+        } else {
+            return {
+                num: parseInt(e.match(/\d+/)),
+                type: e.replace(/[0-9.]/g, '').replace(/bags?/g,'').trim()
+            }
+        }
+
     })
 
-    bagsAllowed[0] === 'no other' ? bagsAllowed = null : '';
 
-    bagRules[bag1.trim()] = bagsAllowed
+
+    bagRules[bag1] = contains[0] === null ? null : contains
 }
 
 
-data.map(e => processRuleLine(e))
 
 
 function checkContains(rule, target) {
@@ -26,10 +32,14 @@ function checkContains(rule, target) {
         return false
     }
 
-    if (bagRules[rule].includes(target)) {
+    let typesInside = bagRules[rule].map(e => {
+        return e.type
+    })
+
+    if (typesInside.includes(target)) {
         return true
     } else {
-        let isAllowed = bagRules[rule].filter(e => {
+        let isAllowed = typesInside.filter(e => {
             return checkContains(e, target)
         })
 
@@ -42,6 +52,21 @@ let part1 = Object.keys(bagRules).filter(e => {
 })
 console.log('Part 1: ', part1.length);
 
+
 // Part 2:
-// Keep track of both number and type of bag in processing
-// 
+function checkNumber(type) {
+    if (bagRules[type] === null) {
+        return 0
+    } 
+    
+    let count = 0
+    for (bag of bagRules[type]) {
+        count += bag.num
+        count += bag.num * checkNumber(bag.type)
+    }
+
+    return count
+
+}
+
+console.log('Part 2: ', checkNumber('shiny gold'))
