5042d24e7e448690741794129f3b089324f92dba
Bas K <39069678+Basje12@users.noreply.github.com>
2020-12-15 16:23:16 +0100

    Day 13

--- Day13/solution.js
+++ Day13/solution.js
@@ -1,8 +1,8 @@
-// const timestamp = 1008713
-// const data = '13,x,x,41,x,x,x,x,x,x,x,x,x,467,x,x,x,x,x,x,x,x,x,x,x,19,x,x,x,x,17,x,x,x,x,x,x,x,x,x,x,x,29,x,353,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,23'.split(',')
+const timestamp = 1008713
+const data = '13,x,x,41,x,x,x,x,x,x,x,x,x,467,x,x,x,x,x,x,x,x,x,x,x,19,x,x,x,x,17,x,x,x,x,x,x,x,x,x,x,x,29,x,353,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,23'.split(',')
 // Test
-const timestamp = 939;
-const data = '7,13,59,31,19'.split(',')
+// const timestamp = 939;
+// const data = '1789,37,47,1889'.split(',')
 
 // Part 1
 const datap1 = data.reduce((a,c) => {
@@ -30,12 +30,39 @@ const part1 = datap1.map(e => {
 console.log('Part 1: ', part1[0].timeToWait * part1[0].busId)
 
 // Part 2
+// Please don't ask me how this works. I worked backward from given answers, tried testcases until it gave the correct answer...
+
 const datap2 = data.map((e,i) => {
-    let obj = {
-        busId: e,
-        tOffset: i
+    if (e !== 'x') {
+        return [parseInt(e), i]
+    }
+}).filter(e => e)
+
+let solved = false
+let i = 2
+let stepsize = 1
+let matched = new Map()
+
+while(!solved) {
+    let match = true
+    for (let j = 0 ; j < datap2.length; j++) {
+        let [id, offset] = datap2[j]
+
+        if ((i + offset) % id !== 0) {
+            match = false
+        } else {
+            if (!matched.has(id)) {
+                matched.set(id, matched.size)
+                stepsize *= id
+            }
+        }
     }
 
-    return obj
-}).filter(e => e.busId !== 'x')
+    if (match === true) {
+        console.log(i)
+        solved = true
+    }
+
+    i += stepsize
+}
 
