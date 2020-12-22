0d7e0c8b5a4c957644b30d153b10e3e8a862eb79
Bas K <39069678+Basje12@users.noreply.github.com>
2020-12-22 18:08:34 +0100

    Day 16, Part 1

---
+++ Day16/solution.js
@@ -0,0 +1,38 @@
+const { verify } = require('crypto');
+const fs = require('fs')
+const data = fs.readFileSync('./input.txt', 'utf-8');
+
+const rules = data.match(/\d+-\d+/g).map(e => {
+    return e.split('-').map(Number)
+})
+const ticketLines = data.split('\r\n').filter(e => e.match(/\d+,\d+/g))
+const myTicket = ticketLines.shift()
+
+let part1Count = 0
+const validTickets = ticketLines.filter(e => verifyTicket(e))
+
+console.log(ticketLines.length, validTickets.length)
+console.log('Part 1: ', part1Count)
+
+function verifyTicket(ticket) {
+    let arr = ticket.split(',').map(Number)
+    let valid = false
+
+    for (num of arr) {
+        let numValid = false
+
+        for (rule of rules) {
+            let [min,max] = rule
+            if (min <= num && num <= max) {
+                valid = true
+                numValid = true
+            }
+        }
+
+        if (numValid === false) {
+            part1Count += num
+        }
+    }
+
+    return valid
+}
