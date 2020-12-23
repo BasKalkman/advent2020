230f2c1a3b45cd9d3e0e41f79215f52b742a7977
Bas K <39069678+Basje12@users.noreply.github.com>
2020-12-23 16:21:47 +0100

    Day 14, P2, WiP

--- Day14/solution.js
+++ Day14/solution.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const data = fs.readFileSync('./input.txt', 'utf-8').split('\r\n');
+const datap2 = [...data]
 
 // Part 1
 function numTo36BitString(num) {
@@ -46,9 +47,31 @@ for (let i = 0; i < data.length; i++) {
 
 let totalP1 = 0
 registers.forEach((a) => totalP1 += a)
-console.log(totalP1)
+console.log('Part 1: ', totalP1)
 
 // Part 2
+const registersP2 = new Map()
+mask = ''
+for (let i = 0; i < datap2.length; i++) {
+    let [instruction, value] = datap2[i].split(' = ')
+    
+    if (instruction === 'mask') {
+        mask = value
+    } else {
+        let masks = generateMasks(mask);
+        let register = instruction.match(/\d+/)[0];
+
+        masks.forEach(m => {
+            let r = processMask(register, m)
+            registersP2.set(r, value)
+        })
+    }
+}
+
+let resultP2 = 0;
+registersP2.forEach(a => resultP2 += parseInt(a))
+console.log('Part 2: ', resultP2)
+
 function generateStates(n){
   var states = [];
 
@@ -65,24 +88,21 @@ function generateStates(n){
 }
 
 
-function doTest() {
-    let test = '111XXX1';
-    let a = generateStates(3)
-    let permutations = a.map(e => e.split(''))
-    let result = []
-    console.log(permutations)
+function generateMasks(mask) {
+    let numOfX = mask.match(/X/g).length
+    let states = generateStates(numOfX).map(e => e.split(''))
+    let results = []
 
-    for (let i = 0; i < permutations.length; i++) {
-        let b = test
-
-        while (b.match(/X/)) {
-            b =   b.replace('X', permutations[i].pop())
+    for (let i = 0; i < states.length; i++) {
+        let temp = mask
+        let arr = states[i]
+        while (temp.match(/X/)) {
+            temp = temp.replace('X', arr.pop())
         }
-
-        result.push(b)
+        results.push(temp)
     }
-
-    console.log(result)
+    
+    return results
 }
 
-doTest()
+// 945115037994 incorrect for p2
