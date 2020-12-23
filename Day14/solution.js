d72885e48b89f705bf0356a2f0ef6fb120164412
Bas K <39069678+Basje12@users.noreply.github.com>
2020-12-23 11:40:21 +0100

    Day 14, part 2, WiP

--- Day14/solution.js
+++ Day14/solution.js
@@ -47,3 +47,42 @@ for (let i = 0; i < data.length; i++) {
 let totalP1 = 0
 registers.forEach((a) => totalP1 += a)
 console.log(totalP1)
+
+// Part 2
+function generateStates(n){
+  var states = [];
+
+  // Convert to decimal
+  var maxDecimal = parseInt("1".repeat(n),2);
+
+  // For every number between 0->decimal
+  for(var i = 0; i <= maxDecimal; i++){
+    // Convert to binary, pad with 0, and add to final results
+    states.push(i.toString(2).padStart(n,'0'));
+  }
+
+  return states;
+}
+
+
+function doTest() {
+    let test = '111XXX1';
+    let a = generateStates(3)
+    let permutations = a.map(e => e.split(''))
+    let result = []
+    console.log(permutations)
+
+    for (let i = 0; i < permutations.length; i++) {
+        let b = test
+
+        while (b.match(/X/)) {
+            b =   b.replace('X', permutations[i].pop())
+        }
+
+        result.push(b)
+    }
+
+    console.log(result)
+}
+
+doTest()
