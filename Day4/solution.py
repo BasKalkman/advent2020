cb18683cae40933a00c75b4253690e272c4adb02
Bas K <39069678+Basje12@users.noreply.github.com>
2020-12-31 15:11:00 +0100

    Day 4, Python

--- Day4/solution.py
+++ Day4/solution.py
@@ -5,9 +5,10 @@ passports = [x.replace('\n', ' ')
 
 required_fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
 
-# Part 1
 valid_passport_count = 0
+valid_passport_count_part2 = 0
 for passport in passports:
+    # Part 1
     fields = {}
     for field in re.findall('(.{3}):', passport):
         fields[field] = 1
@@ -21,4 +22,52 @@ for passport in passports:
     if valid:
         valid_passport_count += 1
 
+        # Part 2
+        for req_field in required_fields:
+            text = re.search(
+                rf'{req_field}:[\w\d#]+', passport).group().split(':')[1]
+
+            if req_field == 'byr':
+                if not 1920 <= int(text) <= 2002:
+                    valid = False
+                    break
+            elif req_field == 'iyr':
+                if not 2010 <= int(text) <= 2020:
+                    valid = False
+                    break
+            elif req_field == 'eyr':
+                if not 2020 <= int(text) <= 2030:
+                    valid = False
+                    break
+            elif req_field == 'hgt':
+                num = int(re.match(r'\d+', text).group())
+                if re.search(r'cm', text):
+                    if not 150 <= num <= 193:
+                        valid = False
+                        break
+                elif num and re.search(r'in', text):
+                    if not 59 <= num <= 76:
+                        valid = False
+                        break
+                else:
+                    valid = False
+                    break
+            elif req_field == 'hcl':
+                if not re.search(r'#[0-9a-f]{6}', text):
+                    valid = False
+                    break
+            elif req_field == 'ecl':
+                if text not in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']:
+                    valid = False
+                    break
+            elif req_field == 'pid':
+                if not len(text) == 9 or not re.match(r'\d{9}', text):
+                    valid = False
+                    break
+
+        if valid:
+            valid_passport_count_part2 += 1
+
+
 print(f'Part 1: {valid_passport_count}')
+print(f'Part 2: {valid_passport_count_part2}')
