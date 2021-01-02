0d72dd854a1bb29ab7b6e02b2e39ff7d323e6cad
Bas K <39069678+Basje12@users.noreply.github.com>
2021-01-02 23:57:57 +0100

    Day 10

--- Day10/solution.py
+++ Day10/solution.py
@@ -1,3 +1,5 @@
+import time
+
 data = sorted([int(x) for x in open('./input.txt').read().splitlines()])
 data = [0] + data
 data.append(data[-1] + 3)
@@ -11,3 +13,27 @@ for i in range(len(data)-1):
         ones += 1
 
 print(f'Part 1: {ones * threes}')
+
+# Part 2
+
+checked = {}
+
+
+def check_adapter(idx):
+    if idx == len(data) - 1:
+        return 1
+    if idx in checked:
+        return checked[idx]
+
+    ans = 0
+    for i in range(idx+1, len(data)):
+        if data[i] - data[idx] <= 3:
+            ans += check_adapter(i)
+        else:
+            break
+
+    checked[idx] = ans
+    return ans
+
+
+print(f'Part 2: {check_adapter(0)}')
