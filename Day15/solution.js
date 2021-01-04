d1d03880ec12ae0ecce48b293fdd2ea736bc2458
Bas K <39069678+Basje12@users.noreply.github.com>
2021-01-04 20:07:08 +0100

    Changed seen from Map() to Uint32Array()

--- Day15/solution.js
+++ Day15/solution.js
@@ -1,24 +1,28 @@
+// Changed seen from Map() to Uint32Array(), which is apparently way faster
+console.time('test')
+
 const input = '16,1,0,18,12,14,19'.split(',').map(Number)
 // const targetTurn = 2020 // Part 1
 const targetTurn = 30000000 // Part 2
 
 let currentValue = input[input.length - 1]
 let currentTurn = input.length
-let seen = new Map()
+let seen = new Uint32Array(targetTurn)
 
-input.map((e,i) => seen.set(e, i+1))
-console.log(seen)
+input.map((e,i) => seen[e] = i+1)
 
 while (currentTurn < targetTurn) {
-    if (seen.has(currentValue)) {
-        let prevTurn = seen.get(currentValue)
-        seen.set(currentValue, currentTurn)
+    if (seen[currentValue] !== 0) {
+        let prevTurn = seen[currentValue]
+        seen[currentValue] = currentTurn
         currentValue = currentTurn - prevTurn;
     } else {
-        seen.set(currentValue, currentTurn)
+        seen[currentValue] = currentTurn
         currentValue = 0
     }
 
     currentTurn++
 }
 console.log(currentValue)
+
+console.timeEnd('test')
